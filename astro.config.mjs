68f1d8a159eac4e249b2ef4cde97740e51aebeb7
Lewis Campbell <github.emptier308@passmail.net>
2026-09-18 16:39:09 +0100

    Updated some re-directs adding new entertainment

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,5 +15,29 @@ export default defineConfig({
       status: 301,
       destination: "/entertainment"
     },
+    "/entertainement/assassins-creed-ii" : {
+      status: 301,
+      destination: "/entertainment/assassins-creed-ii/"
+    },
+    "/entertainement/fallout" : {
+      status: 301,
+      destination: "/entertainment/fallout/"
+    },
+    "/entertainement/halo-3" : {
+      status: 301,
+      destination: "/entertainment/halo-3/"
+    },
+    "/entertainement/grand-theft-auto-v" : {
+      status: 301,
+      destination: "/entertainment/grand-theft-auto-v/"
+    },
+    "/?p=/fact/4apgKuSTXIjUTinnDkaGD9" : {
+      status: 301,
+      destination: "/"
+    },
+    "/?search={search_term_string}" : {
+      status: 301,
+      destination: "/"
+    },
   }
 });
